c2085bc4ee3f025407d604c93418377e4de6fb96
moyyi jayasri <moyyijayasri90@gmail.com>
2026-09-22 16:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ Give a short, clear and accurate answer.
 """
 
     response = client.models.generate_content(
-        model="gemini-3.6-flash",
+        model="gemini-3.5-flash",
         contents=prompt
     )
 
